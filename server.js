758374d3e93ca0d41abdc27ef8a36befd181dd89
jairomartinez1994 <61437470+jairomartinez1994@users.noreply.github.com>
2020-05-23 16:21:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,95 +2,80 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 
-
-var server = express();
+var app = express();
 var PORT = 8080;
-server.use(express.static('public'))
 
-var noteData = [];
+let notesData = [];
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Develop")));
 
+app.get("/api/notes", function (err, res) {
+  try {
+    notesData = fs.readFileSync("Develop/db/db.json");
 
+    notesData = JSON.parse(notesData);
 
-
-
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
-
-server.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
-});
-server.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
-});
-server.get("/api/notes", function (req, res) {
-    
-    res.sendFile(path.join(__dirname, "./db/db.json"))
-});
-
-server.get("/api/notes/:id", function (req, res) {
-    var choosen = req.params;
-    var myid=choosen.id
-    
-    console.log("choosen....",myid);
-   
-     for(var i=0; i<noteData.length;i++){
-        if(choosen===noteData[i].title){
-           return res.json(noteData[i]);
-        }
-     }
+  } catch (err) {
+    console.log(err);
+  }
+  res.json(notesData);
 });
 
 
-server.post("/api/notes", function (req, res) {
-    var newNote = req.body;
-    var noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    console.log("new data enter",newNote);
-    var newNote = req.body;
-    
-    var uniqueId = (noteData.length).toString();
-    
-    newNote.id = uniqueId;
-    console.log("new data enter", newNote);
-    
+app.post("/api/notes", function (req, res) {
+  try {
+    notesData = fs.readFileSync("./Develop/db/db.json");
 
-    noteData.push(newNote);
-    console.log("inside the array",noteData);
-    
-    fs.writeFile("./db/db.json", JSON.stringify(noteData));
-    res.sendFile(path.join(__dirname, "./db/db.json"))
+    notesData = JSON.parse(notesData);
+    req.body.id = notesData.length;
+    notesData.push(req.body);
+    notesData = JSON.stringify(notesData);
+
+    fs.writeFile("./Develop/db/db.json", notesData, function (err) {
+      if (err) throw err;
+    });
+    res.json(JSON.parse(notesData));
+
+  } catch (err) {
+    throw err;
+  }
+});
+
+
+app.delete("/api/notes/:id", function (req, res) {
+  try {
+    notesData = fs.readFileSync("./Develop/db/db.json");
+    notesData = JSON.parse(notesData);
+    notesData = notesData.filter(function (note) {
+      return note.id != req.params.id;
+    });
+    notesData = JSON.stringify(notesData);
+
+    fs.writeFile("./Develop/db/db.json", notesData, function (err) {
+      if (err) throw err;
     });
 
-    
-    
+    res.send(JSON.parse(notesData));
 
-    server.delete("/api/notes/:id", function (req, res) {
-        console.log("Called")
-    
-        var noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-        var choosen = req.params.id;
-        console.log(choosen)
-    
-        var newId = 0;
-        if (noteData.length > 0) {
-            noteData = noteData.filter(currentNote => {
-                return currentNote.id != choosen;
-            });
-            for (currentNote of noteData) {
-                currentNote.id = newId.toString();
-                newId++;
-            }
-            fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
-            res.sendFile(path.join(__dirname, "./db/db.json"));
-        }else return;
-        
-    });
+  } catch (err) {
+    throw err;
+  }
+});
 
-    server.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
-    });
-    
 
-server.listen(PORT, function () {
-    console.log("Listening on PORT" + PORT);
-})
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/api/notes", function (req, res) {
+  return res.sendFile(path.json(__dirname, "./db/db.json"));
+});
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
